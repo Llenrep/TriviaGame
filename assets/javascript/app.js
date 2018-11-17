d0e665e37a5a6458d7218fc3cf349c11065a8182
Llenrep <lppernell1998@gmai.com>
2018-11-17 18:09:43 -0500

    updated to javascript to work within app.js, fixed styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,65 @@
+function timer(){
+                
+                
+}
+
+function result(){
+
+    
+}
+
+
+function question1(){
+    console.log("Question 1")
+    $("#questions").html('<h2>"How Do You fucking spell questionaire?"</h2>')
+    $("#pot-ans-1").html("<p>Answer 1</p>")
+    $("#pot-ans-2").html("<p>Answer 2</p>")
+    $("#pot-ans-3").html("<p>Answer 3</p>")
+    $("#pot-ans-4").html("<p>Answer 4</p>")
+    
+}
+
+function question2(){
+    console.log("Question 2")
+
+}
+
+function question3(){
+    console.log("Question 3")
+
+}
+
+function question4(){
+    console.log("Question 4")
+
+}
+
+function question5(){
+    console.log("Question 5")
+
+}
+
+function question6(){
+    console.log("Question 6")
+
+}
+
+function question7(){
+    console.log("Question 7")
+
+}
+
+function question8(){
+    console.log("Question 8")
+
+}
+
+
+$(document).ready(function(){
+    
+    var amountCorrect = 0;
+    var amountWrong = 0;
+
+    question1();
+
+})
