7e6a1643af5632343b8344c5e7fc99a3edd5fd4b
Llenrep <lppernell1998@gmai.com>
2018-11-17 23:12:17 -0500

    made the result screen. timer needs work

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ For this trivia game, the user will have 10 seconds to answer 8 questions.
 At the end of the game, the user will be prompted with the questions that were correct and the
 questions that were wrong.
 
-the way i am trying to do it, every 10 seconds, the div will change having the same structure, but different
+The way I am trying to do it, every 10 seconds, the div will change having the same structure, but different
 questions.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,124 @@
+var amountCorrect = 0;
+var amountWrong = 0;
+
 function timer(){
-    var x = setInterval(function() {
+    setTimeout(function(){
+        var x = setInterval(function() {
         alert("alert WHADDUP")
-      }, 10000);                      
+      }, 10000); 
+    },100)                   
 }
 
-function result(){
-
+function giphyinsert(){ //we fill it in later when we get the first question to work
 
 }
 
+function result(){ //just formats the page to show a "results" screen
+    $("#buttons").html(""); //clears the buttons from the container
+    $(".timer").html("") //clears the timer as it isn't necessary anymore
+
+    $(".Correct").text("Your amount correct: " + amountCorrect)
+    $(".Wrong").text("Your amount wrong: " + amountWrong)
+    $("#questions").html('<h2>Congratulations, You got through the little quiz! Here are your results!</h2>'); 
+    // Displays the final message along with the amount correct and wrong. If we get far enough, we can add another thing
+}
+
+// Okay
+// We set the value for the correct answer
+// first we need the buttons to be able to do something when clicked
 
 function question1(){
     console.log("Question 1")
-    $("#questions").html('<h2>"Do You Remeber? The..."</h2>')
-    var ans1 = $("#pot-ans-1").html("<p>21st Night of September...</p>")
-    var ans2 = $("#pot-ans-2").html("<p>The Way...</p>")
-    var ans3 = $("#pot-ans-3").html("<p>How the stars stole the night away...</p>")
-    var ans4 = $("#pot-ans-4").html("<p>My thoughts are with you...</p>")
+    $("#questions").html('<h2>"Do You Remeber? The..."</h2>');
+    var ans1 = $("#pot-ans-1").html("<p>21st Night of September...</p>");
+    var ans2 = $("#pot-ans-2").html("<p>The Way...</p>");
+    var ans3 = $("#pot-ans-3").html("<p>How the stars stole the night away...</p>");
+    var ans4 = $("#pot-ans-4").html("<p>My thoughts are with you...</p>");
+
+    var userChoice;
+
+    correctAnswer = ans1;
+
+    $("#pot-ans-1").on("click", function(){
+        console.log("choice 1")
+        userChoice = ans1;
+
+    });
+
+    // $("#pot-ans-2").on("click", function(){
+
+    //     userChoice = ans2;
+
+    // });
+
+    // $("#pot-ans-3").on("click", function(){
+
+    //     userChoice = ans3;
+
+    // });
+
+    // $("#pot-ans-4").on("click", function(){
+
+    //     userChoice = ans4;
+
+    // });
+
+    if (userChoice === correctAnswer){
+        amountCorrect++;
+        console.log("Correct Answer!")
+    } //else {
+    //     amountWrong++;
+    //     console.log("Wrong Answer!")
+    // }
   
-}
+};
 
 function question2(){
-    console.log("Question 2")
+    console.log("Question 2");
 
 }
 
 function question3(){
-    console.log("Question 3")
+    console.log("Question 3");
 
 }
 
 function question4(){
-    console.log("Question 4")
+    console.log("Question 4");
 
 }
 
 function question5(){
-    console.log("Question 5")
+    console.log("Question 5");
 
 }
 
 function question6(){
-    console.log("Question 6")
+    console.log("Question 6");
 
 }
 
 function question7(){
-    console.log("Question 7")
+    console.log("Question 7");
 
 }
 
 function question8(){
-    console.log("Question 8")
+    console.log("Question 8");
 
 }
 
 
 $(document).ready(function(){
     
-    var amountCorrect = 0;
-    var amountWrong = 0;
+   // for (i = 0; i < 9; i++){
+
+    // }
 
     question1();
-    // timer();
+    
+    timer();
+
+    result();
 
 })
