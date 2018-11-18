34f4531881db54a9be4121350fedffe4f4f90078
Llenrep <lppernell1998@gmai.com>
2018-11-17 19:00:42 -0500

    added timer in html and centered

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,23 @@
 function timer(){
-                
-                
+    var x = setInterval(function() {
+        alert("alert WHADDUP")
+      }, 10000);                      
 }
 
 function result(){
 
-    
+
 }
 
 
 function question1(){
     console.log("Question 1")
-    $("#questions").html('<h2>"How Do You fucking spell questionaire?"</h2>')
-    $("#pot-ans-1").html("<p>Answer 1</p>")
-    $("#pot-ans-2").html("<p>Answer 2</p>")
-    $("#pot-ans-3").html("<p>Answer 3</p>")
-    $("#pot-ans-4").html("<p>Answer 4</p>")
-    
+    $("#questions").html('<h2>"Do You Remeber? The..."</h2>')
+    var ans1 = $("#pot-ans-1").html("<p>21st Night of September...</p>")
+    var ans2 = $("#pot-ans-2").html("<p>The Way...</p>")
+    var ans3 = $("#pot-ans-3").html("<p>How the stars stole the night away...</p>")
+    var ans4 = $("#pot-ans-4").html("<p>My thoughts are with you...</p>")
+  
 }
 
 function question2(){
@@ -61,5 +62,6 @@ $(document).ready(function(){
     var amountWrong = 0;
 
     question1();
+    // timer();
 
 })
