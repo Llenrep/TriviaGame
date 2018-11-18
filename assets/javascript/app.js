c9b1d2d68c5056c9a0fca4da0efde29b0e88b580
Llenrep <lppernell1998@gmai.com>
2018-11-17 23:38:13 -0500

    visual studio was freezing but timers underway

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,32 @@
 var amountCorrect = 0;
 var amountWrong = 0;
+var time = 10;
 
-function timer(){
+function timer() {
     setTimeout(function(){
-        var x = setInterval(function() {
-        alert("alert WHADDUP")
-      }, 10000); 
-    },100)                   
+        time--;
+        var mins = Math.floor(time/10/60);
+        var secs = Math.floor(time/10);
+        var tenths = time % 10;
+        document.getElementById("timer").innerHTML = mins + ":" + secs;
+        //     var x = setInterval(function() {
+        //     alert("alert WHADDUP");
+        //     console.log("whats really going on");
+        //   }, 5000); 
+        timer();
+    },100);                   
 }
 
-function giphyinsert(){ //we fill it in later when we get the first question to work
+function giphyInsert(){ //we fill it in later when we get the first question to work
 
 }
 
 function result(){ //just formats the page to show a "results" screen
     $("#buttons").html(""); //clears the buttons from the container
-    $(".timer").html("") //clears the timer as it isn't necessary anymore
+    $(".timer").html(""); //clears the timer as it isn't necessary anymore
 
-    $(".Correct").text("Your amount correct: " + amountCorrect)
-    $(".Wrong").text("Your amount wrong: " + amountWrong)
+    $(".Correct").text("Your amount correct: " + amountCorrect);
+    $(".Wrong").text("Your amount wrong: " + amountWrong);
     $("#questions").html('<h2>Congratulations, You got through the little quiz! Here are your results!</h2>'); 
     // Displays the final message along with the amount correct and wrong. If we get far enough, we can add another thing
 }
@@ -28,7 +36,7 @@ function result(){ //just formats the page to show a "results" screen
 // first we need the buttons to be able to do something when clicked
 
 function question1(){
-    console.log("Question 1")
+    console.log("Question 1");
     $("#questions").html('<h2>"Do You Remeber? The..."</h2>');
     var ans1 = $("#pot-ans-1").html("<p>21st Night of September...</p>");
     var ans2 = $("#pot-ans-2").html("<p>The Way...</p>");
@@ -65,7 +73,7 @@ function question1(){
 
     if (userChoice === correctAnswer){
         amountCorrect++;
-        console.log("Correct Answer!")
+        console.log("Correct Answer!");
     } //else {
     //     amountWrong++;
     //     console.log("Wrong Answer!")
@@ -111,14 +119,12 @@ function question8(){
 
 $(document).ready(function(){
     
-   // for (i = 0; i < 9; i++){
 
-    // }
 
-    question1();
-    
+    // question1();
+
     timer();
 
-    result();
+    // result();
 
 })
