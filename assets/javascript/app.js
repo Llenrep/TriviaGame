978c717dd59691c08a8a204848ce0fddab894285
Llenrep <lppernell1998@gmai.com>
2018-11-18 21:07:29 -0500

    changes in app.js including timer and new array of functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var amountCorrect = 0;
 var amountWrong = 0;
-var time = 100;
+var time = 10;
 
 var userChoice;
 var correctAnswer;
@@ -8,40 +8,39 @@ var correctAnswer;
 var queryURL;
 var APIkey;
 
-function timer() {
-    setTimeout(function(){
+var arrayOfQuestions = [
+    intro,
+    question1,
+    // question2(),
+    // question3(),
+    // question4(),
+    // question5(),
+    // question6(),
+    // question7(),
+    // question8(),
+    result
+]
 
-        time--; //its a countdown so we are looking for decerements specifically
-        
-        var secs = Math.floor(time/10);
-        var tenths = time % 10;
+// what i might try to do is create this array of functions as each function is a question, and go off of that timer/
 
-        document.getElementById("timer").innerHTML = secs + ":" + tenths; //I have no need for the minutes portion, its been opted out
 
-        timer();
-
-        if(secs === -1) {
-            time = 100;
-            $("#timer").html("");
-            alert("10");
-            // clearTimeout(function(){  
-            // })     
-        }
-
-    },100);                   
+function intro() {
+    console.log("Intro")
+    $("#questions").html('<h6>Welcome, In this game, You will be presented with a small portion of a song and your goal is to guess the next couple of lyrics. Good Luck</h6>');
 }
 
-function giphyInsert(){ //we fill it in later when we get the first question to work
+function giphyInsert() { //we fill it in later when we get the first question to work
 
 }
 
-function result(){ //just formats the page to show a "results" screen
+function result() { //just formats the page to show a "results" screen
+    console.log("Result")
     $("#buttons").html(""); //clears the buttons from the container
     $(".timer").html(""); //clears the timer as it isn't necessary anymore
 
     $(".Correct").text("Your amount correct: " + amountCorrect);
     $(".Wrong").text("Your amount wrong: " + amountWrong);
-    $("#questions").html('<h2>Congratulations, You got through the little quiz! Here are your results!</h2>'); 
+    $("#questions").html('<h2>Congratulations, You got through the little quiz! Here are your results!</h2>');
     // Displays the final message along with the amount correct and wrong. If we get far enough, we can add another thing
 }
 
@@ -54,97 +53,108 @@ function result(){ //just formats the page to show a "results" screen
 // scratch that, just choose the one that will increase your score and the ones that wont,
 // i have a good feeling ill run into issues with this later but im gonna go with it. push
 
-function question1(){
-    console.log("Question 1");
+function question1() {
+
+    console.log("question 1")
     $("#questions").html('<h2>"Do You Remeber? The..."</h2>');
     var ans1 = $("#pot-ans-1").html("<p>21st Night of September...</p>");
-    // var cred1 = something;
     var ans2 = $("#pot-ans-2").html("<p>The Way...</p>");
     var ans3 = $("#pot-ans-3").html("<p>How the stars stole the night away...</p>");
     var ans4 = $("#pot-ans-4").html("<p>My thoughts are with you...</p>");
 
-    // var userChoice;
+    // correctAnswer = ans1; //equal to the actual html
 
-    correctAnswer = ans1; //equal to the actual html
-
-    $("#pot-ans-1").on("click", function() {
-
+    $("#pot-ans-1").on("click", function () {
         userChoice = ans1;
-
         amountCorrect++;
-
     });
 
-    $("#pot-ans-2").on("click", function(){
-
+    $("#pot-ans-2").on("click", function () {
         userChoice = ans2;
-
         amountWrong++;
-
     });
 
-    $("#pot-ans-3").on("click", function(){
-
+    $("#pot-ans-3").on("click", function () {
         userChoice = ans3;
-
         amountWrong++;
-
     });
 
-    $("#pot-ans-4").on("click", function(){
-
+    $("#pot-ans-4").on("click", function () {
         userChoice = ans4;
-
         amountWrong++;
-
     });
-  
 };
 
-function question2(){
+function question2() {
     console.log("Question 2");
 
 }
 
-function question3(){
+function question3() {
     console.log("Question 3");
 
 }
 
-function question4(){
+function question4() {
     console.log("Question 4");
 
 }
 
-function question5(){
+function question5() {
     console.log("Question 5");
 
 }
 
-function question6(){
+function question6() {
     console.log("Question 6");
 
 }
 
-function question7(){
+function question7() {
     console.log("Question 7");
 
 }
 
-function question8(){
+function question8() {
     console.log("Question 8");
 
 }
 
+function timer() {
+    console.log("Yuuuhhhh");
 
-$(document).ready(function(){
+    setTimeout(function () {
+
+        time--; //its a countdown so we are looking for decerements specifically
+
+        var secs = Math.floor(time / 10);
+
+        var tenths = time % 10;
+
+        document.getElementById("timer").innerHTML = secs + ":" + tenths;
+
+        timer();
+
+        if (secs === 0) {
+
+            time = 100;
+            $("#timer").html("");
+            alert("Next Question");
+
+        };
+
+    }, 1000);
+
+}
+
+function callFunctions(arr){
+    for (i = 0; i < arr.length; i++) {
+        arr[i]();
+    } 
+}
+
+$(document).ready(function () { //so as of right now, we got the function to run with timer, problem:
     
-
-
-    question1();
-
-    // timer();
-
-    // result();
+    setInterval(callFunctions(arrayOfQuestions), 10000);
 
 })
